e836f161876925d74e0eed6e062621865b3d9663
Hunter Wilkinson <157230944+hunterw455@users.noreply.github.com>
2024-09-20 17:11:37 -0500

    Create Sort.java
    Added the insertion sort for Java

---
+++ Sort.java
@@ -0,0 +1,44 @@
+import java.util.Random;
+
+public class Sort {
+
+    public static void sort(int array[]) {
+        for (int i = 1; i < array.length; i++) {
+            int key = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
+        }
+    }
+
+    static void printArray(int array[]) {
+        int n = array.length;
+        for (int i = 0; i < n; i++) {
+            System.out.print(array[i] + " ");
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 100;
+        Random random = new Random();
+        while (n <= 1500000) {
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++) {
+                array[i] = random.nextInt(10000);
+            }
+
+            long startTime = System.nanoTime();
+            sort(array);
+            long endTime = System.nanoTime();
+
+            double totalTime = (endTime - startTime) / 1_000_000.0;
+            System.out.println(totalTime);
+            n *= 2;
+        }
+    }
+}
